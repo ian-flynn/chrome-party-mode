60afdbc3879ad9f560e3542a0e20486ecb0a23e1
Ian Flynn <ianflynnwork@gmail.com>
2023-03-02 17:12:43 -0500

    new class buttons working, + and -

--- background.js
+++ background.js
@@ -1,47 +1,33 @@
-// // chrome.browserAction.onClicked.addListener(changeColor)
-
-
-// function changeColor (){
-//     console.log('hello')
-//     chrome.tabs.executeScript( {
-//         code: 'document.body.classList("background")'
-//     })
-// }
-
-document.getElementById("sepia-toggle").addEventListener("click", function() {
-  const onOff = document.getElementById("sepia-toggle").value;
-  if(onOff === 'on') document.getElementById("sepia-toggle").setAttribute('value', 'off');
-  if(onOff === 'off') document.getElementById("sepia-toggle").setAttribute('value', 'on');
-
-  chrome.tabs.query({ currentWindow: true}, function(tabs) {
-    tabs.forEach(function(tab) {
-      chrome.tabs.sendMessage(tab.id, { action: "toggle_sephia", onOff: onOff});
-  });
-});
-})
-
-document.getElementById("dark-toggle").addEventListener("click", function() {
-  const onOff = document.getElementById("dark-toggle").value;
-  if(onOff === 'on') document.getElementById("dark-toggle").setAttribute('value', 'off');
-  if(onOff === 'off') document.getElementById("dark-toggle").setAttribute('value', 'on');
-
-  console.log('dark toggle works')
-  chrome.tabs.query({ currentWindow: true}, function(tabs) {
-    tabs.forEach(function(tab) {
-      chrome.tabs.sendMessage(tab.id, { action: "dark_toggle", onOff: onOff});
-  });
-});
-})
-
-document.getElementById("disco-toggle").addEventListener("click", function() {
-  const onOff = document.getElementById("disco-toggle").value;
-  if(onOff === 'on') document.getElementById("disco-toggle").setAttribute('value', 'off');
-  if(onOff === 'off') document.getElementById("disco-toggle").setAttribute('value', 'on');
-  
-  chrome.tabs.query({ currentWindow: true}, function(tabs) {
-    tabs.forEach(function(tab) {
-      chrome.tabs.sendMessage(tab.id, { action: "disco_toggle", onOff: onOff});
-  });
-});
-})
-
+const sepias = document.getElementsByClassName('sepia-change')
+for(let i = 0; i < sepias.length; i++){
+  sepias[i].addEventListener('click', function(e){
+    const onOff = e.target.value;
+    chrome.tabs.query({ currentWindow: true}, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { action: "sepia", onOff: onOff});
+      });
+    });
+  })
+}
+const darks = document.getElementsByClassName('dark-change')
+for(let i = 0; i < darks.length; i++){
+  darks[i].addEventListener('click', function(e){
+    const onOff = e.target.value;
+    chrome.tabs.query({ currentWindow: true}, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { action: "dark", onOff: onOff});
+      });
+    });
+  })
+}
+const raves = document.getElementsByClassName('rave-change')
+for(let i = 0; i < sepias.length; i++){
+  raves[i].addEventListener('click', function(e){
+    const onOff = e.target.value;
+    chrome.tabs.query({ currentWindow: true}, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { action: "rave", onOff: onOff});
+      });
+    });
+  })
+}
--- scripts/content.js
+++ scripts/content.js
@@ -1,61 +1,52 @@
+const keyframes = `@keyframes rave {
+  0% {background-color: red;}
+  10% {background-color: orange;}
+  20% {background-color: yellow;}
+  30% {background-color: green;}
+  40% {background-color: blue;}
+  50% {background-color: indigo;}
+  60% {background-color: violet;}
+  70% {background-color: black;}
+  80% {background-color: white;}
+  90% {background-color: pink;}
+  100% {background-color: red;}
+}`;
+const style = document.createElement("style");
+style.type = "text/css";
+style.appendChild(document.createTextNode(keyframes));
+document.head.appendChild(style);
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "toggle_sephia") {
-    console.log('action sepia works')
-
-    if(request.onOff === 'on'){
-      const giant = document.createElement("div")
-      giant.setAttribute('id', 'giant-mask')
-      giant.setAttribute('style', 'z-index: 1000; width: 100%; min-height: 100%; background-color: red; position: fixed; top: 0; left: 0; opacity: 0.5;')
-      let body = document.querySelector('body')
-      body.appendChild(giant)
-    } else {
-      document.querySelector('#giant-mask').remove()
+function makeMask(onOff, action){
+  if(onOff === 'on'){
+    const color =  action === "sepia" ? '#7F462C' : 'black';
+    const mask = document.createElement("div")
+    mask.setAttribute('id', `${action}-mask`)
+    mask.style['z-index'] = '1000';
+    mask.style.width = '100%';
+    mask.style['min-height'] = '100%'; 
+    mask.style['background-color'] = `${color}`;
+    mask.style.position = 'fixed'; 
+    mask.style.top = '0'; 
+    mask.style.left = '0'; 
+    mask.style.opacity = '0.5';
+    mask.style['pointer-events'] = 'none';
+    if(action === "rave"){
+      mask.style.animation = 'rave 1s infinite';
     }
-  } else if (request.action === "dark_toggle") {
-    console.log('action dark works')
-
-    if(request.onOff === 'on'){
-      const anotherGiant = document.createElement("div")
-      anotherGiant.setAttribute('id', 'another-giant-mask')
-      anotherGiant.setAttribute('style', 'z-index: 1000; width: 100%; min-height: 100%; background-color: black; position: fixed; top: 0; left: 0; opacity: 0.5;')
-      let body = document.querySelector('body')
-      body.appendChild(anotherGiant)
-    } else {
-      document.querySelector('#another-giant-mask').remove()
-    }
+    document.querySelector('body').appendChild(mask)
+  } else if(onOff === 'off') {
+    document.querySelector(`#${action}-mask`).remove()
+  }
+}
 
 
-  } else if (request.action === "disco_toggle") {
-
-    if(request.onOff === 'on'){
-      console.log('action disco works')
-      const oneMoreGiant = document.createElement("div")
-      oneMoreGiant.setAttribute('id', 'one-more-giant-mask')
-      oneMoreGiant.setAttribute('style', 'z-index: 1000; width: 100%; min-height: 100%; background-color: black; position: fixed; top: 0; left: 0; opacity: 0.5;animation: rave 1s infinite;')
-      let body = document.querySelector('body')
-      body.appendChild(oneMoreGiant)
-      const keyframes = `
-      @keyframes rave {
-        0% {background-color: red;}
-        10% {background-color: orange;}
-        20% {background-color: yellow;}
-        30% {background-color: green;}
-        40% {background-color: blue;}
-        50% {background-color: indigo;}
-        60% {background-color: violet;}
-        70% {background-color: black;}
-        80% {background-color: white;}
-        90% {background-color: pink;}
-        100% {background-color: red;}
-      }`;
-      const style = document.createElement("style");
-      style.type = "text/css";
-      style.appendChild(document.createTextNode(keyframes));
-      document.head.appendChild(style);
-    } else {
-      document.querySelector('#one-more-giant-mask').remove()
-    }
+chrome.runtime.onMessage.addListener(function(request) {
+  if (request.action === "sepia") {
+    makeMask(request.onOff, request.action)
+  } else if (request.action === "dark") {
+    makeMask(request.onOff, request.action)
+  } else if (request.action === "rave") {
+    makeMask(request.onOff, request.action)
   }
 });
 
